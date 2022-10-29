4a0c46ecfcfbd68bfd96f488c2f13adf71c7cc4a
Yauheni <yauheni.domashevsky@gmail.com>
2022-10-29 03:41:03 -0700

    delete RuBasePage

--- src/main/java/pages/CardLibraryPage.java
+++ src/main/java/pages/CardLibraryPage.java
@@ -6,11 +6,8 @@ import com.codeborne.selenide.SelenideElement;
 import static com.codeborne.selenide.Selenide.$$x;
 import static com.codeborne.selenide.Selenide.$x;
 
-public class CardLibraryPage extends RuBasePage {
+public class CardLibraryPage extends EuBasePage {
 
-    public SelenideElement classesMenu = $x("//div/div/h6[contains(text(),'Все классы')][1]");
-    public SelenideElement demonhunterButton = $x("//h6[contains(text(),'Охотник на демонов')]");
-    public SelenideElement druidButton = $x("//h6[contains(text(),'Друид')]");
     public SelenideElement inputSearch = $x("//input[@id=\"searchInput\"]");
     public SelenideElement manaSearchButton = $x("//button[@class=\"ManaControl__ManaCrystal-sc-12tfrw2-2 gXbfGO\"]/h4[contains(text(),'0')]");
     public SelenideElement izeraType = $x("//li[contains(text(), 'Тип существа')]/span[@class='value']");
@@ -41,18 +38,6 @@ public class CardLibraryPage extends RuBasePage {
 
 
     public CardLibraryPage() {
-    }
-
-    public SelenideElement getClassesMenu() {
-        return classesMenu;
-    }
-
-    public SelenideElement getDemonhunterButton() {
-        return demonhunterButton;
-    }
-
-    public SelenideElement getDruidButton() {
-        return druidButton;
     }
 
     public SelenideElement getInputSearch() {
--- src/test/java/uiTest/SearchCardTest.java
+++ src/test/java/uiTest/SearchCardTest.java
@@ -1,43 +1,17 @@
 package uiTest;
 
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.Keys;
-import pages.EuBasePage;
-import pages.RuBasePage;
 import pages.CardLibraryPage;
 
 import static com.codeborne.selenide.Selenide.$x;
 
 public class SearchCardTest extends BaseTest{
 
-    private String oneManaName = "Изера";
-    private String expectedType = "Дракон";
     static CardLibraryPage cardLibraryPage = new CardLibraryPage();
-    RuBasePage ruBasePage = new RuBasePage();
-    EuBasePage euBasePage = new EuBasePage();
-    private static final String RUURL = "https://hearthstone.blizzard.com/ru-ru";
-    private static final String EUURL = "https://hearthstone.blizzard.com/en-us";
     private SelenideElement searchingCard = $x("//img[@alt=\"Вызов\"]");
     BaseTestMethod baseTestMethod = new BaseTestMethod();
-
-    @Test
-    public void  searchTest(){
-        Selenide.open(RUURL);
-        ruBasePage.getCardMenuButton().click();
-        ruBasePage.getCardLibraryMenu().click();
-        cardLibraryPage.getInputSearch().click();
-        cardLibraryPage.getInputSearch().sendKeys(oneManaName, Keys.ENTER);
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        cardLibraryPage.getCardImageButton().click();
-        Assert.assertEquals(cardLibraryPage.getIzeraType().getText(), expectedType);
-    }
 
     @Test
     public void dhBeastCardCollectionTest(){
