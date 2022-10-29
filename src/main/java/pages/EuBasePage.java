3937ec8b56284020f4d260b842db2e28217f0e77
Yauheni <yauheni.domashevsky@gmail.com>
2022-10-29 02:04:48 -0700

    change cardButton path

--- src/main/java/pages/EuBasePage.java
+++ src/main/java/pages/EuBasePage.java
@@ -5,6 +5,6 @@ import com.codeborne.selenide.SelenideElement;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class EuBasePage {
-    public SelenideElement cardButton = $x("//span[contains(text(),'Cards')]");
+    public SelenideElement cardButton = $x("//li[@class='Menu-item Menu-item--3 Menu-item--cards has-dropdown']");
     public SelenideElement cardLibraryButton = $x("//a[contains(text(),'Card Library')]");
 }
